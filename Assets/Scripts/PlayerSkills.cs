c537350145c803f3e6ee2196309d0a9e3fc81d64
David Strachan <davidstrachan92@gmail.com>
2022-01-29 17:59:33 +0000

    Add job billboard popup

--- Assets/Scripts/PlayerSkills.cs
+++ Assets/Scripts/PlayerSkills.cs
@@ -1,5 +1,3 @@
-using System;
-using Jobs;
 using Model;
 using TMPro;
 using UnityEngine;
@@ -15,55 +13,24 @@ public class PlayerSkills : MonoBehaviour
     public TextMeshProUGUI charismaValue;
     public Button charismaButton;
 
-    // Jobs
-    public Button carpetFactoryButton;
-    public Button sellDrugsButton;
-    public Button commitFraudButton;
-    public Button mugPeopleButton;
-    public Button robBankButton;
-    public Button stealIdentitiesButton;
-
     // Time
+    public TextMeshProUGUI timeValue;
     public Slider gameSpeedSlider;
 
     // Misc
     public Button resetButton;
 
-    // Test
-    public TextMeshProUGUI timeValue;
-    private Job _job;
-
     private static GameState GameState => GameState.Instance;
-
-    private void Awake()
-    {
-        _job = GetComponent<Job>();
-    }
 
     private void Start()
     {
-        strengthButton.onClick.AddListener(() => GameState.Strength.Xp++);
-        intelligenceButton.onClick.AddListener(() => GameState.Intelligence.Xp++);
-        charismaButton.onClick.AddListener(() => GameState.Charisma.Xp++);
-
-        // carpetFactoryButton.onClick.AddListener(() => );
-        // sellDrugsButton.onClick.AddListener(() => );
-        // commitFraudButton.onClick.AddListener(() => );
-        // mugPeopleButton.onClick.AddListener(() => );
-        // robBankButton.onClick.AddListener(() => );
-        // stealIdentitiesButton.onClick.AddListener(() => );
+        strengthButton.onClick.AddListener(() => GameState.Strength.Xp += 10);
+        intelligenceButton.onClick.AddListener(() => GameState.Intelligence.Xp += 10);
+        charismaButton.onClick.AddListener(() => GameState.Charisma.Xp += 10);
 
         gameSpeedSlider.onValueChanged.AddListener(value => GameState.Time.Factor = value);
 
         resetButton.onClick.AddListener(() => GameState.Reset());
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _job.Attempt();
-        }
     }
 
     private void OnGUI()
