c0c3af26c146f607b49917036590f5cd3a3b7a37
David Strachan <davidstrachan92@gmail.com>
2022-01-29 23:42:34 +0000

    Update stats panel

---
+++ Assets/Stats/StatsSetter.cs
@@ -0,0 +1,28 @@
+using Model;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsSetter : MonoBehaviour
+{
+    public TextMeshProUGUI strengthValue;
+    public Button strengthButton;
+    public TextMeshProUGUI intelligenceValue;
+    public Button intelligenceButton;
+    public TextMeshProUGUI charismaValue;
+    public Button charismaButton;
+
+    private void Start()
+    {
+        strengthButton.onClick.AddListener(() => GameState.Instance.Strength.Xp += 10);
+        intelligenceButton.onClick.AddListener(() => GameState.Instance.Intelligence.Xp += 10);
+        charismaButton.onClick.AddListener(() => GameState.Instance.Charisma.Xp += 10);
+    }
+
+    private void OnGUI()
+    {
+        strengthValue.text = $"{GameState.Instance.Strength.Value:n}";
+        intelligenceValue.text = $"{GameState.Instance.Intelligence.Value:n}";
+        charismaValue.text = $"{GameState.Instance.Charisma.Value:n}";
+    }
+}
