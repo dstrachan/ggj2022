403de2a18931d3af17949b7ebfb13f755ced93a7
David Eichmann <EichmannD@gmail.com>
2022-02-13 15:28:35 +0000

    Keyboard movement is camera aligned

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -70,77 +70,39 @@ namespace Player
         {
             if (!TimeWarp.TimeIsWarping)
             {
-                var mouseInputX = Input.GetAxis("Horizontal");
-                var mouseinputY = Input.GetAxis("Vertical");
-                var mouseAction = Input.GetButton("Jump");
-                bool usingMouse = mouseInputX != 0 || mouseinputY != 0 || mouseAction;
-                var joyLInputX = Input.GetAxis("LJoyHorizontal");
-                var joyLInputY = Input.GetAxis("LJoyVertical");
-                // var joyRInputX = Input.GetAxis("RJoyHorizontal");
-                // var joyRInputY = Input.GetAxis("RJoyVertical");
-                var joyinputAction = Input.GetButtonDown("JoyJump");
-                bool usingJoy = joyLInputX != 0
-                                || joyLInputY != 0
-                                // || joyRInputY != 0
-                                // || joyRInputY != 0
-                                || joyinputAction;
-                _usingJoyPad = usingJoy || (!usingMouse && _usingJoyPad);
-
-                if (_usingJoyPad)
+                if (Camera.main != null)
                 {
-                    if (Camera.main != null)
-                    {
-                        // Joy Pad Controls
+                    var inputMoveX = Input.GetAxis("MoveHorizontal");
+                    var inputMoveY = Input.GetAxis("MoveVertical");
+                    var action = Input.GetButton("Action");
+                    
+                    // Movement direction Depends on camera angle
+                    var cameraForward = Camera.main.transform.forward;
+                    var dirForward = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                    var dirRight = Vector3.Cross(dirForward, Vector3.down);
 
-                        // Depends on camera angle
-                        var cameraForward = Camera.main.transform.forward;
-                        var dirForward = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-                        var dirRight = Vector3.Cross(dirForward, Vector3.down);
+                    // Movement
+                    _moveDirection = ((dirRight * inputMoveX) + (dirForward * inputMoveY)) * speed;
+                    _controller.Move(_moveDirection * Time.deltaTime);
 
-                        // Movement
-                        _moveDirection = ((dirRight * joyLInputX) + (dirForward * joyLInputY)) * speed;
-                        _controller.Move(_moveDirection * Time.deltaTime);
-
-                        // Facing
-                        if (_moveDirection != Vector3.zero)
-                            transform.LookAt(transform.position + _moveDirection);
-                    }
-                }
-                else
-                {
-                    // Keyboard and Mouse Controls
-                    var inputX = mouseInputX;
-                    var inputY = mouseinputY;
-
-                    var inputModifyFactor = (inputX != 0.0f && inputY != 0.0f) ? .7071f : 1.0f;
-
+                    // Facing
+                    // TODO face second joy stick direction if using joy pad.
+                    if (_moveDirection != Vector3.zero)
+                        transform.LookAt(transform.position + _moveDirection);
+                    
+                    // What are we mousing over?
                     var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-
                     var hits = Physics.RaycastAll(ray);
                     Vector3 groundHit = transform.position;
-                    if (hits.Any())
+                    foreach (var hit in hits)
                     {
-                        foreach (var hit in hits)
+                        if (hit.collider.CompareTag(Tags.Ground))
                         {
-                            
-                            if (hit.collider.CompareTag(Tags.Ground))
-                            {
-                                groundHit = hit.point;
-                                var transform1 = transform;
-                                transform.LookAt(new Vector3(hit.point.x, transform1.position.y, hit.point.z));
-                                break;
-                            }
+                            groundHit = hit.point;
+                            var transform1 = transform;
+                            transform.LookAt(new Vector3(hit.point.x, transform1.position.y, hit.point.z));
+                            break;
                         }
-                    }
-                    
-
-                    _moveDirection = new Vector3(inputX * inputModifyFactor, -1, inputY * inputModifyFactor);
-                    _moveDirection = transform.TransformDirection(_moveDirection) * speed;
-
-                    // Dont move too close to target and go crazy
-                    if (Vector3.Distance(groundHit, transform.position) > 1.6f)
-                    {
-                        _controller.Move(_moveDirection * Time.deltaTime);
                     }
                 }
             }
